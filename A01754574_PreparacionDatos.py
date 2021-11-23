0cf4fd2b648ac3cd5154d3cb9063b9b2f9ad05ab
a01754574 <a01754574@itesm.mx>
2021-11-22 23:41:18 -0600

    Commit Save

--- A01754574_PreparacionDatos.py
+++ A01754574_PreparacionDatos.py
@@ -10,14 +10,14 @@ from sklearn.metrics import mean_squared_error as ms
 from sklearn.metrics import mean_absolute_error as ma
 
 
-data = pd.read_csv("/home/yayo/Documentos/Programación/Python/CienciaDeDatos/DatosComida.csv", encoding="utf-8", encoding_errors="ignore")
+data = pd.read_csv("DatosComida.csv", encoding="utf-8", encoding_errors="ignore")
 
 print(data.shape)
 print(data.head())
 print(data.describe())
 
 
-x = ['Calorias (kcal)', 'Carbohidratos (g)', 'Lipidos (g)', 'Proteina (g)', 'Sodio (mg)']
+x = ['Carbohidratos (g)', 'Lipidos (g)', 'Proteina (g)', 'Sodio (mg)']
 X =  data[x]
 
 Y = data['Calorias (kcal)']
@@ -53,6 +53,38 @@ def plot(X, Y, model):
     plt.show()
 
 plot(X,Y,model)
+
+
+def main(campo: str, dataset):
+    
+    x = np.array(list(dataset["Calorias (kcal)"])).reshape(-1, 1)
+    print(x)
+
+    y = np.array(list(dataset[campo]))
+    print(y)
+
+    plt.scatter(x, y)
+    
+    model = lr()
+    model.fit(x, y)
+
+    y_pred = model.predict(x)
+
+    plt.plot(x, y_pred)
+
+    plt.xlabel("Calorias (kcal)")
+    plt.ylabel(campo)
+
+    plt.show()
+    
+
+dataset = data.sort_values(by = "Calorias (kcal)")
+
+for campo in list(dataset):
+    if campo not in ["Calorias (kcal)", "Nombre"]:
+        main(campo, dataset)
+
+
 
 
 
